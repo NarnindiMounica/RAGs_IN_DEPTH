34219388bce20bb9a9cd64ae8ddec971218d81d5
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-01-24 22:58:43 +0530

    react_nodes class classic rag function

--- Project/DOC_RAG/src/nodes/react_nodes.py
+++ Project/DOC_RAG/src/nodes/react_nodes.py
@@ -10,3 +10,16 @@ from langchain_core.messages import HumanMessage
 from langchain_community.utilities import WikipediaAPIWrapper
 from langchain_community.tools.wikipedia.tool import WikipediaQueryRun
 
+class ReactRAGNodes:
+    "contains the node functions for RAG workflow"
+
+    def __init__(self, retriever, llm):
+        self.retriever = retriever
+        self.llm = llm
+        self._agent = None #Lazy init agent
+
+    def  retrieve_docs(self, state:RAGState)->RAGState:
+        "classic retrivere node"
+        docs = self.retriever.invoke(state.question) 
+        return RAGState(question=state.question,
+                        retrieved_docs= docs) 
